faad07736b1d292197d2f36c81e53a6ed4f916b9
AGoblinKing <AGoblinKing@isekai.dev>
2019-03-20 15:32:44 -0700

    cooler logo

--- SHOP/HTTP_REPLY/index.js
+++ SHOP/HTTP_REPLY/index.js
@@ -1,11 +1,10 @@
-```js
 export default ({
     HTTP,
     HTTP_REPLY
 }) => {
     Object.entries(HTTP_REPLY).
         forEach(([ path, response ]) => 
-            HTTP.get(path, (res) => res.send(response))
+            HTTP.get(path, (res) => 
+                res.send(response))
         );
 };
-```
