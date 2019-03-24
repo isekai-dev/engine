5f7e111e436f4d95ced9b052cfa02395215cc4b5
AGoblinKing <AGoblinKing@isekai.dev>
2019-03-24 01:43:46 -0700

    Early commit on dependencies, trying out the template work

---
+++ src/commands/shop.js
@@ -0,0 +1,11 @@
+export default ({
+    command: `shop [add|remove] [shop]`,
+    help: `Add a shop to pull skills from`,
+
+    handler: () => {
+        /*
+         * npm install --save [shop]
+         * npm uninstall --save [shop]
+         */
+    }
+});
